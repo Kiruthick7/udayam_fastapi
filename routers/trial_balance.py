2b75f4cfc322e91223f48452e48dbe062773044e
Kiruthick-B <kiruthick.b@hy-vee.com>
2025-12-31 02:15:35 +0530

    add print

--- routers/trial_balance.py
+++ routers/trial_balance.py
@@ -62,6 +62,8 @@ def get_trial_balance(
 
                 for result in cursor.stored_results():
                     for row in result.fetchall():
+                        print(row);
+
                         category = row.get("category")
                         amount = float(row.get("amount") or 0)
                         acc_type = row.get("type")
